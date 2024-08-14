854fd851fe045a28ad2c6fe599169f2b703a4645
Anseongmin5739 <smahn4069@gmail.com>
2024-08-14 09:42:39 +0900

    수정 : 매핑 성공

--- src/pages/Manager/SurveyDetail.js
+++ src/pages/Manager/SurveyDetail.js
@@ -6,25 +6,34 @@ const SurveyDetail = () => {
   const { curriculumId, surveyId } = useParams(); // useParams에서 curriculumId와 surveyId를 받아옴
 
   const [surveyDetails, setSurveyDetails] = useState(null);
+  const [curriculumSimple, setCurriculumSimple] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const getToken = () => localStorage.getItem("access-token");
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchSurveyDetail = async () => {
       try {
         const token = getToken();
         const config = {
           headers: { access: token },
         };
 
-        // 설문조사 상세 정보 가져오기
-        const response = await axios.get(
-          `/managers/curriculum/${curriculumId}/survey/${surveyId}/detail`,
+        // 설문조사 진행 중인 데이터 가져오기
+        const surveyResponse = await axios.get(
+          `/managers/curriculum/${curriculumId}/survey-status/progress`,
           config
         );
-        setSurveyDetails(response.data);
+
+        // 교육과정 간단 정보 가져오기
+        const curriculumResponse = await axios.get(
+          `/managers/curriculum/${curriculumId}/survey-status/curriculum-simple`,
+          config
+        );
+
+        setSurveyDetails(surveyResponse.data);
+        setCurriculumSimple(curriculumResponse.data);
       } catch (error) {
         setError(error.response?.data || "데이터 가져오기 오류");
       } finally {
@@ -32,8 +41,8 @@ const SurveyDetail = () => {
       }
     };
 
-    fetchData();
-  }, [curriculumId, surveyId]);
+    fetchSurveyDetail();
+  }, [curriculumId]);
 
   if (isLoading) {
     return <div>로딩 중...</div>;
@@ -43,17 +52,18 @@ const SurveyDetail = () => {
     return <div>오류 발생: {error}</div>;
   }
 
-  if (!surveyDetails) {
+  if (!surveyDetails || !curriculumSimple) {
     return <div>설문조사 정보를 불러올 수 없습니다.</div>;
   }
 
   return (
     <div className="survey-detail">
       <h2 className="survey-detail-title">
-        {surveyDetails.curriculumName} {surveyDetails.curriculumTh}기 설문조사
+        {curriculumSimple.name} {curriculumSimple.th}기 설문조사
       </h2>
-      <h3 className="survey-detail-subtitle">{surveyDetails.surveyTitle}</h3>
+      <h3 className="survey-detail-subtitle">{surveyDetails.title}</h3>
       <div className="survey-cards-container">
+        <p>진행중인 설문조사: {surveyDetails.completed} / {surveyDetails.total}</p>
         {/* 설문조사와 관련된 추가적인 정보를 여기서 표시 */}
       </div>
     </div>
