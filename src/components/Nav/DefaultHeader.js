4e79514baa69c1ed6fd35c9bfa0110544137641a
Anseongmin5739 <smahn4069@gmail.com>
2024-08-16 14:15:08 +0900

    수정 : 헤더부분 수정완료

--- src/components/Nav/DefaultHeader.js
+++ src/components/Nav/DefaultHeader.js
@@ -5,29 +5,31 @@ import "./DefaultHeader.css";
 const DefaultHeader = () => {
   const [token, setToken] = useState(localStorage.getItem("access-token"));
 
-  if (!token) {
-    return (
-      <header id="default_header">
-        <div className="default_h-inner">
-          <div className="default_h-left">
-            <h1 className="default_h-logo">
-              <a className="default_navbar-brand" href="/">
-                <img
-                  src="/images/logo/HomeLearn_Default_Header_Logo.png"
-                  alt="로고"
-                />
-              </a>
-            </h1>
-          </div>
-          <div className="default_h-right">
+  return (
+    <header id="default_header">
+      <div className="default_h-inner">
+        <div className="default_h-left">
+          <h1 className="default_h-logo">
+            <a className="default_navbar-brand" href="/">
+              <img
+                src="/images/logo/HomeLearn_Default_Header_Logo.png"
+                alt="로고"
+              />
+            </a>
+          </h1>
+        </div>
+        <div className="default_h-right">
+          {token ? (
+            <span className="default_h-user_info">Welcome!</span>
+          ) : (
             <Link to="/login" className="default_h-login_btn">
               로그인
             </Link>
-          </div>
+          )}
         </div>
-      </header>
-    );
-  }
+      </div>
+    </header>
+  );
 };
 
 export default DefaultHeader;
