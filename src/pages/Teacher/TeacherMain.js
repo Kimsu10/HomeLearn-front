dc865279a9e60e1bb553373d36f4aa55de2abedf
Noh Seung Been <shtmdqls990921@gmail.com>
2024-08-18 15:43:11 +0900

    수정 - Teacher 페이지 경로 수정 중

--- src/pages/Teacher/TeacherMain.js
+++ src/pages/Teacher/TeacherMain.js
@@ -1,64 +1,74 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { useNavigate, Route, Routes } from "react-router-dom";
+import {jwtDecode} from "jwt-decode";
 import "./TeacherMain.css";
+import TeacherCalendar from "../../components/Calendar/TeacherCalendar/TeacherCalendar";
 import Question from "./Question";
 import Lecture_State from "./Lecture_State";
-import TeacherCalendar from "../../components/Calendar/TeacherCalendar/TeacherCalendar";
 import Today_It from "./Today_It";
 import Faq from "./Faq";
-import { Route, Routes } from "react-router-dom";
 
 import TeacherSideBar from "../../components/SideBar/TeacherSideBar";
-import TeacherContact from "../Manager/TeacherContact";
-import StudentContact from "../Manager/StudentContact";
-import Notice from "../Manager/Notice";
-import CurriculumManagement from "../Manager/CurriculumManagement";
-import TeacherManagement from "../Manager/TeacherManagement";
 import TeacherHeader from "../../components/Nav/TeacherHeader";
 
+
 function TeacherDashBoard() {
-  return (
-      <>
-        <h1>대시보드</h1>
-        <div className="teacher-dashboard-grid-container">
-          <div className="teacher-dashboard-grid">
-            <Lecture_State />
-            <Question />
-
-            <Faq />
-          </div>
-          <div className="teacher-dashboard-grid2">
-            <TeacherCalendar />
-            <Today_It />
-          </div>
-        </div>
-      </>
-  );
+    return (
+        <>
+            <h1>대시보드</h1>
+            <div className="teacher-dashboard-grid-container">
+                <div className="teacher-dashboard-grid">
+                    <Lecture_State />
+                    <Question />
+                    <Faq />
+                </div>
+                <div className="teacher-dashboard-grid2">
+                    <TeacherCalendar />
+                    <Today_It />
+                </div>
+            </div>
+        </>
+    );
 }
 
-function TeacherMain() {
-  return (
-    <div className="teacher-App">
-      <TeacherHeader />
-      <div className="teacher-main-content">
-        <TeacherSideBar />
-        <div className="teacher-content-area">
-          <Routes>
-            <Route path="" element={<TeacherDashBoard />} />
+const TeacherMain = () => {
+    const [selectedSubject, setSelectedSubject] = useState(null);
+    const [username, setUsername] = useState("");
 
+    const navigate = useNavigate();
 
-            <Route
-              path="manage-curriculums"
-              element={<CurriculumManagement />}
-            />
-            <Route path="manage-teachers" element={<TeacherManagement />} />
-            <Route path="notice" element={<Notice />} />
-            <Route path="contact-students" element={<StudentContact />} />
-            <Route path="contact-teachers" element={<TeacherContact />} />
-          </Routes>
+    useEffect(() => {
+        const token = localStorage.getItem("access-token");
+
+        try {
+            const decodedToken = jwtDecode(token);
+            setUsername(decodedToken.username);
+        } catch (error) {
+            console.error("jwt token 해석 실패 : ", error);
+        }
+    }, []);
+
+    console.log(username);
+
+    return (
+        <div className="teacher-App">
+            <TeacherHeader />
+            <div className="teacher-main-content">
+                <TeacherSideBar />
+                <div className="teacher-content-area">
+                    <Routes>
+                        <Route path="" element={<TeacherDashBoard />} />
+                        {/*<Route path="/:subjectName/board"*/}
+                        {/*       element={*/}
+                        {/*        <TeacherLecture subject={selectedSubject} username={username} />*/}
+                        {/*       }*/}
+                        {/*/>*/}
+
+                    </Routes>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-}
+    );
+};
 
 export default TeacherMain;
