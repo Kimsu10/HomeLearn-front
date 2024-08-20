bdd3b0b0e96e771bdf049891a054f58a047b8f69
Anseongmin5739 <smahn4069@gmail.com>
2024-08-20 19:38:05 +0900

    수정 : 캘린더 연결 성공 css 수정 필요

--- src/components/Calendar/StudentCalendar/StudentCalendar.js
+++ src/components/Calendar/StudentCalendar/StudentCalendar.js
@@ -1,27 +1,142 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import './ManagerCalendarDetail.css';
-import ManagerCalendar from './ManagerCalendar';
-import swal from "sweetalert";
+import React, { useState, useEffect } from 'react';
+import { FaCalendarPlus } from 'react-icons/fa';
+import axios from '../../../utils/axios';
+import './StudentCalendar.css'; // 올바른 CSS 파일 경로로 수정
+import StudentModal from '../../Modal/StudentModal/StudentModal'; // 모달 컴포넌트 이름 수정
 
-const CalendarDetail = () => {
-  const { eventId } = useParams();
-  const navigate = useNavigate();
-  const [event, setEvent] = useState(null);
-  const [events, setEvents] = useState([]);
-  const [selectedEvent, setSelectedEvent] = useState(null);
-  const [editMode, setEditMode] = useState(false);
+const StudentCalendar = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
   const [selectedDate, setSelectedDate] = useState(null);
-
-  const predefinedColors = ['#FF9999', '#99FF99', '#9999FF'];
+  const [events, setEvents] = useState([]);
+  const [holidays, setHolidays] = useState([]);
+  const [newEvent, setNewEvent] = useState({ title: '', start: null, end: null, color: '#FF9999' });
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
-    const storedEvents = JSON.parse(localStorage.getItem('calendarEvents')) || [];
-    setEvents(storedEvents);
-    const foundEvent = storedEvents.find((e) => e.id === Number(eventId));
-    setEvent(foundEvent);
-    setSelectedEvent(foundEvent);
-  }, [eventId]);
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.get('/students/calendar-events');
+        setEvents(response.data);
+      } catch (error) {
+        console.error('Error fetching events:', error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  useEffect(() => {
+    const saveEvents = async () => {
+      try {
+        await axios.post('/students/save-calendar-events', events);
+      } catch (error) {
+        console.error('Error saving events:', error);
+      }
+    };
+
+    saveEvents();
+  }, [events]);
+
+  useEffect(() => {
+    const fetchHolidays = async () => {
+      const year = currentDate.getFullYear();
+      const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+      const serviceKey = "t21Zxd4T5l%2FCFpu9dpVZ2U4nEIv06W14hNeu7Op7HA0yIBHYgMu23%2FL6JHBWQ%2Bp9HNG%2B93RJwgq7zANzmn%2B2%2BA%3D%3D";
+      const url = `http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo?serviceKey=${serviceKey}&pageNo=1&numOfRows=100&solYear=${year}&solMonth=${month}`;
+
+      try {
+        const response = await fetch(url);
+        if (response.ok) {
+          const responseText = await response.text();
+          const parser = new DOMParser();
+          const xmlDoc = parser.parseFromString(responseText, "application/xml");
+          const items = xmlDoc.getElementsByTagName("item");
+          const holidays = Array.from(items)
+            .map((item) => {
+              const locdate = item.getElementsByTagName("locdate")[0].textContent;
+              return locdate;
+            })
+            .filter((date) => {
+              const monthDay = date.substr(4, 4);
+              return monthDay !== "0717";
+            });
+          setHolidays(holidays);
+        } else {
+          console.error("공휴일 데이터 가져오기 실패:", response.statusText);
+        }
+      } catch (error) {
+        console.error("공휴일 데이터 가져오는 중 오류:", error);
+      }
+    };
+
+    fetchHolidays();
+  }, [currentDate]);
+
+  const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+  const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay();
+
+  const generateCalendarDates = () => {
+    const dates = [];
+    const prevMonthLastDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
+    const nextMonthFirstDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1);
+
+    for (let i = firstDayOfMonth - 1; i >= 0; i--) {
+      dates.push({
+        date: new Date(prevMonthLastDate.getFullYear(), prevMonthLastDate.getMonth(), prevMonthLastDate.getDate() - i),
+        isCurrentMonth: false
+      });
+    }
+
+    for (let i = 1; i <= daysInMonth; i++) {
+      dates.push({
+        date: new Date(currentDate.getFullYear(), currentDate.getMonth(), i),
+        isCurrentMonth: true
+      });
+    }
+
+    const remainingDays = 7 - (dates.length % 7);
+    if (remainingDays < 7) {
+      for (let i = 0; i < remainingDays; i++) {
+        dates.push({
+          date: new Date(nextMonthFirstDate.getFullYear(), nextMonthFirstDate.getMonth(), i + 1),
+          isCurrentMonth: false
+        });
+      }
+    }
+
+    return dates;
+  };
+
+  const handleMonthChange = (direction) => {
+    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + direction, 1));
+  };
+
+  const handleOpenModal = () => {
+    setNewEvent({ title: '', start: selectedDate, end: selectedDate, color: '#FF9999' });
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setNewEvent({ title: '', start: null, end: null, color: '#FF9999' });
+  };
+
+  const handleSaveEvent = async () => {
+    if (newEvent.title && newEvent.start && newEvent.end) {
+      try {
+        const response = await axios.post('/students/add-calendar-event', newEvent);
+        setEvents([...events, response.data]);
+        handleCloseModal();
+      } catch (error) {
+        console.error('Error saving event:', error);
+      }
+    }
+  };
+
+  const handleDateClick = (date) => {
+    const adjustedDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+    setSelectedDate(adjustedDate);
+  };
 
   const getEventsForDate = (date) => {
     return events.filter(event =>
@@ -30,148 +145,119 @@ const CalendarDetail = () => {
     );
   };
 
-  if (!event) {
-    return navigate('/managers');
-  }
+  const isCurrentDate = (date) => {
+    const today = new Date();
+    return date && date.getDate() === today.getDate() &&
+           date.getMonth() === today.getMonth() &&
+           date.getFullYear() === today.getFullYear();
+  };
 
-  const handleDeleteEvent = (id) => {
-    swal({
-      title: "삭제하시겠습니까?",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    }).then((willDelete) => {
-      if (willDelete) {
-        const updatedEvents = events.filter((e) => e.id !== id);
-        localStorage.setItem('calendarEvents', JSON.stringify(updatedEvents));
-        setEvents(updatedEvents);
-        if (id === selectedEvent.id) {
-          setSelectedEvent(null);
-          setEditMode(false);
-        }
-        swal("일정이 성공적으로 삭제되었습니다!", {
-          icon: "success",
-        });
-      }
+  const isHoliday = (date) => {
+    return holidays.some((holiday) => {
+      const holidayDate = new Date(
+        holiday.substr(0, 4),
+        holiday.substr(4, 2) - 1,
+        holiday.substr(6, 2)
+      );
+      return holidayDate.toDateString() === date.toDateString();
     });
   };
 
-  const handleEditEvent = (evt) => {
-    setSelectedEvent(evt);
-    setEditMode(true);
-  };
-
-  const handleSaveEvent = () => {
-    swal({
-      title: "변경 사항을 저장하시겠습니까?",
-      icon: "info",
-      buttons: true,
-    }).then((willSave) => {
-      if (willSave) {
-        const updatedEvents = events.map((e) => e.id === selectedEvent.id ? selectedEvent : e);
-        localStorage.setItem('calendarEvents', JSON.stringify(updatedEvents));
-        setEvents(updatedEvents);
-        setEvent(selectedEvent);
-        setEditMode(false);
-        swal("변경 사항이 성공적으로 저장되었습니다!", {
-          icon: "success",
-        });
-      }
-    });
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setSelectedEvent({ ...selectedEvent, [name]: value });
-  };
-
-  const handleDayClick = (date) => {
-    setSelectedDate(date);
-    setSelectedEvent(null);
-    setEditMode(false);
+  const isWeekend = (date) => {
+    const day = date.getDay();
+    return { isSunday: day === 0, isSaturday: day === 6 };
   };
 
   return (
-    <div className="detail-calendar-detail-page">
-      <div className="detail-calendar-detail-sidebar">
-        <ManagerCalendar events={events} onDayClick={handleDayClick} />
-      </div>
-      <div className="detail-calendar-detail-container">
-        <div className="detail-calendar-detail-header">
-          <h2>일정 관리</h2>
-          <button onClick={() => navigate("/managers")}>돌아가기</button>
+    <section className="student-calendar-container">
+      <div className="student-calendar">
+        <div className="student-calendar-header">
+          <button onClick={() => handleMonthChange(-1)}>&lt;</button>
+          <h2>{currentDate.getFullYear()}년 {currentDate.getMonth() + 1}월</h2>
+          <button onClick={() => handleMonthChange(1)}>&gt;</button>
         </div>
-        <div className="detail-calendar-detail-content">
-          <div className="detail-all-events">
-            <ul>
-              {getEventsForDate(selectedDate || new Date(event.start)).map((evt) => (
-                <li key={evt.id} className="detail-event-item">
-                  <div className="detail-event-color" style={{ backgroundColor: evt.color }}></div>
-                  <div className="detail-event-info">
-                    <div className="detail-event-title-container">
-                      <span className="detail-event-title">{evt.title}</span>
-                      <div className="detail-event-actions">
-                        <button onClick={() => handleEditEvent(evt)}>
-                          <i className="fas fa-pencil-alt"></i>
-                        </button>
-                        <button onClick={() => handleDeleteEvent(evt.id)}>
-                          <i className="fas fa-trash-alt"></i>
-                        </button>
-                      </div>
-                    </div>
-                    <div className="detail-event-dates">
-                      <p><strong>시작일:</strong> {new Date(evt.start).toLocaleDateString()}</p>
-                      <p><strong>종료일:</strong> {new Date(evt.end).toLocaleDateString()}</p>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
+        <div className="student-add-list">
+          <button onClick={() => setCurrentDate(new Date())}>Today</button>
+          <button onClick={handleOpenModal}>
+            일정 추가
+            <FaCalendarPlus style={{ marginLeft: '8px' }} />
+          </button>
+        </div>
+        <div className="student-calendar-body">
+          <div className="student-weekdays">
+            {['일', '월', '화', '수', '목', '금', '토'].map(day => (
+              <div key={day}>{day}</div>
+            ))}
           </div>
-
-          {editMode && selectedEvent && (
-            <div className="detail-event-edit">
-              <div className="detail-event-color-picker">
-                {predefinedColors.map(color => (
-                  <div
-                    key={color}
-                    className={`color-option ${selectedEvent.color === color ? 'selected' : ''}`}
-                    style={{ backgroundColor: color }}
-                    onClick={() => setSelectedEvent({ ...selectedEvent, color })}
-                  ></div>
-                ))}
+          <div className="student-days">
+            {generateCalendarDates().map((day, index) => (
+              <div
+                key={index}
+                className={`student-day ${day.isCurrentMonth ? '' : 'other-month'} ${
+                  selectedDate &&
+                  day.date.getFullYear() === selectedDate.getFullYear() &&
+                  day.date.getMonth() === selectedDate.getMonth() &&
+                  day.date.getDate() === selectedDate.getDate() ? 'selected' : ''
+                } ${isCurrentDate(day.date) ? 'current-date' : ''} ${
+                  isHoliday(day.date) ? 'holiday' : ''
+                } ${isWeekend(day.date).isSunday ? 'sunday' : isWeekend(day.date).isSaturday ? 'saturday' : ''}`}
+                onClick={() => handleDateClick(day.date)}
+              >
+                <span className="student-day-number">{day.date.getDate()}</span>
+                <div className="student-events-indicator">
+                  {getEventsForDate(day.date).map(event => (
+                    <div key={event.id} className="event-bar" style={{ backgroundColor: event.color }}>
+                      <span className="student-event-title">{event.title}</span>
+                    </div>
+                  ))}
+                </div>
               </div>
-              <input
-                type="text"
-                name="title"
-                value={selectedEvent.title}
-                onChange={handleChange}
-                className="detail-event-title-input"
-              />
-              <input
-                type="date"
-                name="start"
-                value={selectedEvent.start}
-                onChange={handleChange}
-                className="detail-event-date-input"
-              />
-              <input
-                type="date"
-                name="end"
-                value={selectedEvent.end}
-                onChange={handleChange}
-                className="detail-event-date-input"
-              />
-              <div className="detail-event-actions-edit">
-                <button onClick={handleSaveEvent}>저장<i className="fas fa-save"></i></button>
-                <button onClick={() => handleDeleteEvent(selectedEvent.id)}>삭제<i className="fas fa-trash-alt"></i></button>
-              </div>
-            </div>
-          )}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+
+      <StudentModal isOpen={isModalOpen} onClose={handleCloseModal}>
+        <div>
+          <h3 className="student-modal-add">일정 등록하기</h3>
+          <div className="student-event-form">
+            <label>제목</label>
+            <input
+              type="text"
+              placeholder="일정 제목"
+              value={newEvent.title}
+              onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+            />
+            <label>시작일</label>
+            <input
+              type="date"
+              value={newEvent.start ? newEvent.start.toISOString().substr(0, 10) : ''}
+              onChange={(e) => setNewEvent({ ...newEvent, start: new Date(e.target.value) })}
+            />
+            <label>종료일</label>
+            <input
+              type="date"
+              value={newEvent.end ? newEvent.end.toISOString().substr(0, 10) : ''}
+              onChange={(e) => setNewEvent({ ...newEvent, end: new Date(e.target.value) })}
+            />
+            <div className="student-color-picker">
+              {['#FF9999', '#99FF99', '#9999FF'].map(color => (
+                <div
+                  key={color}
+                  className={`student-color-option ${newEvent.color === color ? 'selected' : ''}`}
+                  style={{ backgroundColor: color }}
+                  onClick={() => setNewEvent({ ...newEvent, color })}
+                ></div>
+              ))}
+            </div>
+            <div className='student-calendar-submit'>
+              <button onClick={handleSaveEvent}>등록</button>
+            </div>
+          </div>
+        </div>
+      </StudentModal>
+    </section>
   );
 };
 
-export default CalendarDetail;
+export default StudentCalendar;
