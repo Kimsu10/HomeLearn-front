0c2cb2a99b207ea8dd4e1a5acb9f9fcbc83905cf
Anseongmin5739 <smahn4069@gmail.com>
2024-08-22 15:35:14 +0900

    수정: 설문조사 없음 표시

--- src/pages/Manager/Survey.js
+++ src/pages/Manager/Survey.js
@@ -5,6 +5,7 @@ import './Survey.css';
 
 const Survey = () => {
   const [surveys, setSurveys] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchSurveys = async () => {
@@ -13,6 +14,8 @@ const Survey = () => {
         setSurveys(response.data);
       } catch (error) {
         console.error('설문조사 정보를 가져오는데 실패했습니다.', error);
+      } finally {
+        setLoading(false); // 로딩 상태 해제
       }
     };
 
@@ -25,24 +28,30 @@ const Survey = () => {
         <h2>설문 조사</h2>
       </div>
       <ul className="survey-list">
-        {surveys.map(survey => (
-          <li key={survey.id} className="survey-item">
-            <div className="survey-info">
-              <span className="survey-id">{survey.th}기</span>
-              <div className="survey-details">
-                <Link to={`/managers/curriculum/${survey.th}/survey/${survey.id}/detail`} className="survey-title-dash">
-                  {survey.title}
-                </Link>
+        {loading ? (
+          <li>로딩 중...</li>
+        ) : surveys.length === 0 ? (
+          <li>설문조사가 없습니다.</li>
+        ) : (
+          surveys.map(survey => (
+            <li key={survey.id} className="survey-item">
+              <div className="survey-info">
+                <span className="survey-id">{survey.th}기</span>
+                <div className="survey-details">
+                  <Link to={`/managers/curriculum/${survey.th}/survey/${survey.id}/detail`} className="survey-title-dash">
+                    {survey.title}
+                  </Link>
+                </div>
               </div>
-            </div>
-            <div className="survey-status">
-              <div className="survey-check">{survey.isCompleted ? '완료' : '진행중'}</div>
-              <span className="survey-participants">
-                <i className="fa-solid fa-user"></i> {survey.participants}/{survey.total}
-              </span>
-            </div>
-          </li>
-        ))}
+              <div className="survey-status">
+                <div className="survey-check">{survey.isCompleted ? '완료' : '진행중'}</div>
+                <span className="survey-participants">
+                  <i className="fa-solid fa-user"></i> {survey.participants}/{survey.total}
+                </span>
+              </div>
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
