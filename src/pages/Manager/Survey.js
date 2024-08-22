d25d6eb986ec0722806690a91960363e2d35d4f0
Anseongmin5739 <smahn4069@gmail.com>
2024-08-22 15:01:28 +0900

    수정: 설문조사 링크 보내기

--- src/pages/Manager/Survey.js
+++ src/pages/Manager/Survey.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Survey.css';
 
-const SettingList = () => {
+const Survey = () => {
   return (
     <div className="setting-list">
       <div className="setting-list-header">
@@ -12,7 +13,9 @@ const SettingList = () => {
           <div className="survey-info">
             <span className="survey-id">1기</span>
             <div className="survey-details">
-              <p className="survey-title-dash">네이버 클라우드 데브옵스 <br />만족도 설문조사 1차</p>
+              <Link to="curriculum/1/survey/detail" className="survey-title-dash"> {/* 해당 설문조사 페이지로 이동 */}
+                네이버 클라우드 데브옵스 <br />만족도 설문조사 1차
+              </Link>
             </div>
           </div>
           <div className="survey-status">
@@ -24,13 +27,16 @@ const SettingList = () => {
           <div className="survey-info">
             <span className="survey-id">2기</span>
             <div className="survey-details">
-              <p className="survey-title-dash">네이버 클라우드 데브옵스 <br />만족도 설문조사 2차</p>
+              <Link to="curriculum/2/survey/detail" className="survey-title-dash">
+                네이버 클라우드 데브옵스 <br />만족도 설문조사 2차
+              </Link>
             </div>
           </div>
           <div className="survey-status">
             <div className="survey-check">완료</div>
             <span className="survey-participants">
-                <i className="fas fa-user"></i> 5/5</span>
+              <i className="fas fa-user"></i> 5/5
+            </span>
           </div>
         </li>
       </ul>
@@ -38,4 +44,4 @@ const SettingList = () => {
   );
 };
 
-export default SettingList;
+export default Survey;
